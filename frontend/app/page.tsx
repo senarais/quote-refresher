feffb74189fe7a353fabf5af6f7efd788be502b8
Abisena <abisena27@gmail.com>
2025-12-27 23:33:24 +0700

    finished app without responsivity

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [quote, setQuote] = useState("this is the quote of the day. enjoy yourself dude.");
+
+  async function fetchQuote() {
+    const res = await fetch("http://localhost:4000/getquote")
+    const result = await res.json()
+    console.log(result);
+    setQuote(result.quote);
+  }
+
   return (
     <div className="w-full h-dvh flex justify-center items-center">
       <div className="flex flex-col items-center gap-10 w-[40%]">
         <h1 className="text-primary text-5xl font-bold">Quote of The Day</h1>
         <div className="relative bg-white border-2 w-full h-60 flex p-10">
-          <p className="text-primary text-5xl leading-14">“this is the quote of the day. enjoy yourself dude.”</p>
-          <div className="absolute -z-2 p-10 border-2 left-2 top-2 bg-white border w-full h-60"></div>
+          <p className="text-primary text-5xl leading-14">"{quote}"</p>
+          <div className="absolute -z-2 p-10 border-2 left-2 top-2 bg-white w-full h-60"></div>
         </div>
-        <a href="" className="bg-[#5170ff] p-3 rounded-full text-white">Generate new quote</a>
+        <a onClick={fetchQuote} className="bg-[#5170ff] cursor-pointer p-3 rounded-full text-white">Generate new quote</a>
       </div>
     </div>
   );
